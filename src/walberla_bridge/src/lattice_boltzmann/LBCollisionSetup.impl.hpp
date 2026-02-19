301e8e2b2cb249c6d93d23899de5cb892b6fc280
Rebecca Stephan <rebecca.stephan@icp.uni-stuttgart.de>
2026-02-19 16:59:50 +0100

    Adapted to tuple viscosity, only first component is used.

--- src/walberla_bridge/src/lattice_boltzmann/LBCollisionSetup.impl.hpp
+++ src/walberla_bridge/src/lattice_boltzmann/LBCollisionSetup.impl.hpp
@@ -30,7 +30,7 @@
 template <typename FloatType, lbmpy::Arch Architecture>
 FloatType
 LBWalberlaImpl<FloatType, Architecture>::shear_mode_relaxation_rate() const {
-  return FloatType{2} / (FloatType{6} * m_viscosity + FloatType{1});
+  return FloatType{2} / (FloatType{6} * m_viscosity[0] + FloatType{1});
 }
 
 template <typename FloatType, lbmpy::Arch Architecture>
