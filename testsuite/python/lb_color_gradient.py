7bb57d44a1546cfab30cb0a98597ae4c76a56d6b
Rebecca Stephan <rebecca.stephan@icp.uni-stuttgart.de>
2026-07-22 13:36:33 +0200

    density is component densities

--- testsuite/python/lb_color_gradient.py
+++ testsuite/python/lb_color_gradient.py
@@ -127,16 +127,16 @@ class ColorGradientLBTest(ut.TestCase):
         self.assertAlmostEqual(visc[1], VISCOSITY, places=10)
 
     def test_node_density_two_component(self):
-        """Getting component_densities on a node should return two values in CG mode."""
+        """Getting density on a node should return two values in CG mode."""
         lbf = self._create_lbf()
-        dens = lbf[0, 0, 0].component_densities
+        dens = lbf[0, 0, 0].density
         self.assertEqual(len(dens), 2)
 
     def test_set_density(self):
-        """Setting per-node component_densities with two values should round-trip."""
+        """Setting per-node density with two values should round-trip."""
         lbf = self._create_lbf()
-        lbf[0, 0, 0].component_densities = [0.7, 0.3]
-        dens = lbf[0, 0, 0].component_densities
+        lbf[0, 0, 0].density = [0.7, 0.3]
+        dens = lbf[0, 0, 0].density
         self.assertAlmostEqual(dens[0], 0.7, places=10)
         self.assertAlmostEqual(dens[1], 0.3, places=10)
 
@@ -160,7 +160,7 @@ class ColorGradientLBTest(ut.TestCase):
         for x in range(0, N, N // 3):
             for y in range(0, N, N // 3):
                 for z in range(0, N, N // 3):
-                    d = lbf[x, y, z].component_densities
+                    d = lbf[x, y, z].density
                     self.assertAlmostEqual(d[0], rho_a[x, y, z], places=10)
                     self.assertAlmostEqual(d[1], rho_b[x, y, z], places=10)
 
@@ -287,8 +287,8 @@ class ColorGradientLBTest(ut.TestCase):
         for x in range(N):
             for y in range(N):
                 for z in range(N):
-                    lbf2[x, y, z].component_densities = [rho_a[x, y, z],
-                                                         rho_b[x, y, z]]
+                    lbf2[x, y, z].density = [rho_a[x, y, z],
+                                             rho_b[x, y, z]]
         lbf2.init_two_component()
         densities_node = np.copy(lbf2[:, :, :].density)
 
